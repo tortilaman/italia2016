e0b991f3cb3e03d178cb14786b444da2bca86a39
tortilaman <tortilaman@gmaill.com>
2016-07-14 02:06:51 -0700

    Search done except for disciplines showing at the beginning.

--- public_html/italiadev/js/designIndex.js
+++ public_html/italiadev/js/designIndex.js
@@ -55,16 +55,43 @@ Y8a     a8P  "8b,   ,aa  88,    ,88  88          "8a,   ,aa  88       88
 ** =====================================================*/
 
 	var $rawData = $("[data-title]"),
-		newArray = new Array();
+		newArray = new Array(),
+		tagsArr = [],
+		wordsToSkip = ['is', 'in'];
+	//Populate newArray with data.
 	$rawData.each(function(int, el) {
-		newArray.push($(this).attr('data-title'));
+		newArray.push({label : $(this).attr('data-title'), category: "Interviewees"});
+		var tags = $(this).attr("data-tags").split(",");
+		tags.forEach(function(element, index, array) {
+			if($.inArray(element, tagsArr) === -1 && element != "") {
+				newArray.push({label : element, category : "Disciplines"});
+				tagsArr.push(element);
+			}
+		});
+	});
+
+	//Sort first by category and then alphabetically.
+	newArray.sort(function(a,b) {
+		if(a.category == "Disciplines" && b.cateogry == "Interviewees") {
+			return -1
+		} else if(a.category == "Interviewees" && b.category == "Disciplines") {
+			return 1
+		} else if(a.category == "Interviewees" && b.category == "Interviewees") {
+			var aName = a.label.split(' ');
+			var bName = b.label.split(' ');
+			if(aName[aName.length - 1] < bName[bName.length - 1]) return -1;
+			if(aName[aName.length - 1] > bName[bName.length - 1]) return 1;
+			return 0;
+		} else if(a.category == "Disciplines" && b.category == "Disciplines") {
+			if(a.label < b.label) return -1;
+			if(a.label > b.label) return 1;
+			return 0;
+		}
 	});
 
 	function showSearch() {
-		console.log("show search");
+//		console.log("show search");
 		searching = true;
-//		$("#search").val(key);
-//		key = "";
 		$('article img').addClass("oHidden");
 		$('article .entry_title').addClass("oHidden");
 		$("#search").addClass("lSearch");
@@ -73,11 +100,11 @@ Y8a     a8P  "8b,   ,aa  88,    ,88  88          "8a,   ,aa  88       88
 			'margin' : '1vw',
 			'padding': '5vw'
 		});
-		$("#d_filter span").removeClass("oHidden");
+		$("#d_filter span").addClass("active");
 	}
 
 	function hideSearch() {
-		console.log("hide search");
+//		console.log("hide search");
 		$('article img').removeClass("oHidden");
 		$('article .entry_title').removeClass("oHidden");
 		$(".design main").css({
@@ -86,26 +113,38 @@ Y8a     a8P  "8b,   ,aa  88,    ,88  88          "8a,   ,aa  88       88
 		});
 		$("#d_filter").css('height', '5vw');
 		$('#search').removeClass("lSearch");
-		$("#d_filter span").addClass("oHidden");
+		$("#d_filter span").removeClass("active");
 		searching = false;
-//		$("#search").blur();
-//		$(window).focus();
 	}
 
+	/* ===========================================================
+		PAGE ITEM FILTER
+	** =========================================================*/
+
 	function $filterResults() {
+		//There is a search term
 		if($("#search").val() != '') {
-			var $notTyped = "article:not([data-title*='"+$("#search").val()+"'])";
-			var $typed = "article[data-title*='"+$("#search").val()+"']";
-			$($notTyped).hide("fast").addClass("oHidden");
-			$($typed).show("fast").removeClass("oHidden");
-		}
+			//Interviewees & Films
+			$("article[data-title]").each(function() {
+				var tags = $(this).attr("data-tags"),
+					names = $(this).attr("data-title").toLowerCase(),
+					//Merge names and tags if there are tags.
+					data = tags == "" ? names : names.concat(" ", tags),
+					show = false;
 
-
-		if($("#search").val() == '') {
-			$("[data-title]").show("fast");
-		}
-
-		if($("#search").val() != '') {
+				var $search = $("#search").val().toLowerCase();//search is the search data
+				var matcher = new RegExp('(^| )' + $search, 'i');
+//					if($search.match("^|"+data)) {
+				if(matcher.test(data)) {
+					show = true;
+				}
+				if(show) {
+					$(this).show("fast").removeClass("oHidden");
+				} else {
+					$(this).hide("fast").addClass("oHidden");
+				}
+			});
+			//Year Titles
 			for( var year = 2012; year < 2016; year++) {
 				//All articles are hidden
 				if(!$("#flex_grid_"+year+" article").not(".oHidden").length) {
@@ -114,44 +153,106 @@ Y8a     a8P  "8b,   ,aa  88,    ,88  88          "8a,   ,aa  88       88
 					$("#year_title_"+year).show("fast");
 				}
 			}
+			//Empty search results. Can't have that!
+			if(!$("[id*='flex_grid_'] article").not(".oHidden").length) {
+				$('main').addClass("noResults");
+			} else {
+				$('main').removeClass("noResults");
+			}
+		}
+		//There's no search term
+		else {
+			$("[data-title]").show("fast");
 		}
 	}
 
-	//AUTOCOMPLETE
+	/* =========================================
+		AUTOCOMPLETE / LIST FILTER
+	** =======================================*/
+
 	$("#search").autocomplete({
 		source: newArray,
+		delay: 0,
 		select: function(event, ui) {
-			console.log("item selected");
-			$("#search").val(ui.item.value);
+			$("#search").val(ui.item.label);
+			console.log($("#search").val());
+			$filterResults();
 			hideSearch();
 			$(window).focus();
-			$filterResults();
 		}
-	});
+	}).data('ui-autocomplete')._renderItem = function( ul, item ) {
+		var term = $('#search').val();
+		var label = item.label
+		.replace(new RegExp('(^| )' + '(' + term + ')', 'ig'), '$1<span class="highlight">$2</span>');
+
+		return $( '<li></li>' )
+			.data( 'item.autocomplete', item )
+			.append( '<a>' + label + '</a>' )
+			.appendTo( ul );
+	};
+
+	$("#search").data('ui-autocomplete')._renderMenu = function( ul, items ) {
+		var that = this,
+			currentCategory = "";
+		$.each( items, function( index, item ) {
+			var li;
+			if ( item.category != currentCategory ) {
+				ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+				currentCategory = item.category;
+			}
+			li = that._renderItemData( ul, item );
+			if ( item.category ) {
+				li.attr( "aria-label", item.category + " : " + item.label );
+			}
+		});
+	}
+
+	// Overrides the default autocomplete filter function to search only from the beginning of the string
+	$.ui.autocomplete.filter = function (array, term) {
+		var matcher = new RegExp('(^| )' + $.ui.autocomplete.escapeRegex(term), 'i');
+		return $.grep(array, function (value) {
+			var val = (value.label)
+			.split(' ') // You'll get an array of all words here
+			.filter(function(word) { return wordsToSkip.indexOf(word) < 0; }) // Remove skip words
+			.join(' '); // Join the remaining words back together
+			return matcher.test(val);
+		});
+	};
+
+	/* =======================================================
+		EVENT LISTENERS
+	** =====================================================*/
+
 	$("#search").focusin(function() {
-		console.log("focusin");
+//		console.log("focusin");
 		$("#search").val("");
 	});
+
 	$("#search").focusout(function() {
-		console.log("focusout");
+//		console.log("focusout");
 		hideSearch();
 	});
 
 	$("#searchForm").submit(function() {
-		console.log("submitForm");
+//		console.log("submitForm");
 		$("#search").blur();
 		return false;
 	});
 
-	$("search").on("autocompleteselect", function(event, ui) {});
-
-	/* =======================================================
-	KEYBOARD EVENT LISTENER
-	** =====================================================*/
-	$("#search").keyup( function() {
-		if(searching == false) {showSearch();}
-		$filterResults();
+	$("#search").click(function() {
+		showSearch();
 	});
+
+	$("#search").on("keyup", function(e) {
+		if(e.keyCode === 27) {
+			$("#search").val("")
+			hideSearch();
+		} else {
+			if(searching == false) {showSearch();}
+			$filterResults();
+		}
+	});
+
 	$(window).on("keyup", function(e) {
 		if(searching == false && e.keyCode >= 65 && e.keyCode <=90) {
 			key = String.fromCharCode(e.keyCode).toLowerCase();
