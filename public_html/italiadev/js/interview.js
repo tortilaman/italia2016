9620c26d56de8d41fc3b586a68c4043674f45c55
tortilaman <tortilaman@gmaill.com>
2016-08-11 04:50:25 -0700

    * Responsive Imagery implimented

--- public_html/italiadev/js/interview.js
+++ public_html/italiadev/js/interview.js
@@ -11,13 +11,33 @@
 
 **===============================*/
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-	$(".v-chapter").mouseover(function() {
-		var index = $(this).attr('data-chapter'),
-			offset = $(this).attr('data-offset');
-		$("#v-chapter-ind").css('margin-top', offset+'em');
-		$("#v-description .inner").addClass("dHidden");
-		$("#v-description .inner:eq("+index+")").removeClass("dHidden");
-	});
+    $(".v-chapter").mouseover(function() {
+        var index = $(this).attr('data-chapter'),
+            offset = $(this).attr('data-offset');
+        $("#v-chapter-ind").css('margin-top', offset + 'em');
+        $("#v-description .inner").addClass("dHidden");
+        $("#v-description .inner:eq(" + index + ")").removeClass("dHidden");
+    });
+
+    /* ==================
+    	GRID FUNCTION
+    ** ================*/
+
+    function offsetGrid() {
+        var hoLast,
+            hoThis,
+            $items = $("#v-context > div");
+        if ($(window).outerWidth() >= "480") {
+            hOffset = ['-1vw', '0', '1vw'];
+            $items.each(function(index, value) {
+                while (hoThis == hoLast) hoThis = hOffset[Math.floor(Math.random() * hOffset.length)];
+                $(this).css('transform', 'translateX(' + hoThis + ')');
+                hoLast = hoThis;
+            });
+        }
+    }
+
+    offsetGrid();
 });
--- public_html/italiadev/js/stackGrid.js
+++ public_html/italiadev/js/stackGrid.js
@@ -1,168 +1,168 @@
 //Function to scale numbers between two ranges.
-Number.prototype.map = function (in_min, in_max, out_min, out_max) {
-	return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+Number.prototype.map = function(in_min, in_max, out_min, out_max) {
+    return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 };
 
 $.fn.extend({
-	animateCss: function (animationName) {
-		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		$(this).addClass('animated ' + animationName).one(animationEnd, function() {
-			$(this).removeClass('animated ' + animationName);
-			$(this).css('opacity', 1);
-		});
-	}
+    animateCss: function(animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+            $(this).css('opacity', 1);
+        });
+    }
 });
 
 /*=================================
 	DOCUMENT READY
 **===============================*/
 
-$(document).ready(function () {
-	//Scrolling Variables
-	var scrollPos,
-		iterator = 0,
-		totalHeight = 0,
-		offset = 0,
-		$entries = $("section[class*='-section']");
+$(document).ready(function() {
+    //Scrolling Variables
+    var scrollPos,
+        iterator = 0,
+        totalHeight = 0,
+        offset = 0,
+        $entries = $("section[class*='-section']");
 
-	/*=================================
-		SCROLLING F'NS
-	**===============================*/
+    /*=================================
+    	SCROLLING F'NS
+    **===============================*/
 
-	function calcHeights() {
-		$entries.each(function () {
-			$(this).attr('data-offset', offset);
-			var $height = $(this).is($entries.first()) ? $(this).outerHeight() : $(this).outerHeight(true);
-			offset += $(this).outerHeight(true);
-			$(this).attr('data-height', $(this).outerHeight(true));
-			$('body').css('height', totalHeight += $height);
-			$(this).css('z-index', $entries.length - iterator);
-			$(this).find(".scrollHide").css('z-index', $entries.length - iterator++);
-		});
-		iterator = offset = totalHeight = 0;
-	}
+    function calcHeights() {
+        $entries.each(function() {
+            $(this).attr('data-offset', offset);
+            var $height = $(this).is($entries.first()) ? $(this).outerHeight() : $(this).outerHeight(true);
+            offset += $(this).outerHeight(true);
+            $(this).attr('data-height', $(this).outerHeight(true));
+            $('body').css('height', totalHeight += $height);
+            $(this).css('z-index', $entries.length - iterator);
+            $(this).find(".scrollHide").css('z-index', $entries.length - iterator++);
+        });
+        iterator = offset = totalHeight = 0;
+    }
 
-	//Delays implementation of scrolling on pages with lots of images, and avoids scrolling script on mobile.
-	if($(window).outerWidth(true) > 700) {
-		if($("#v-context img").length) {
-			$(window).on('load', function() {
-				calcHeights();
-			});
-		} else {
-			calcHeights();
-		}
-	}
+    //Delays implementation of scrolling on pages with lots of images, and avoids scrolling script on mobile.
+    if ($(window).outerWidth(true) > 700) {
+        if ($("#v-context img").length) {
+            $(window).on('load', function() {
+                calcHeights();
+            });
+        } else {
+            calcHeights();
+        }
+    }
 
-	/*=================================
-		PARALLAX SOME DIVS
-	**===============================*/
+    /*=================================
+    	PARALLAX SOME DIVS
+    **===============================*/
 
-	//Parent needs to be a section level element
-	function parallax(parentDiv) {
-		var scrolled 		= $(window).scrollTop(),
-			$parent			= $(parentDiv),
-			$dynDiv 		= $(parentDiv+' > div:nth-of-type(2)'),
-			$scrollRefDiv 	= $(parentDiv + ' + section' );
+    //Parent needs to be a section level element
+    function parallax(parentDiv) {
+        var scrolled = $(window).scrollTop(),
+            $parent = $(parentDiv),
+            $dynDiv = $(parentDiv + ' > div:nth-of-type(2)'),
+            $scrollRefDiv = $(parentDiv + ' + section');
 
-		if (scrolled > $parent.attr('data-offset') && scrolled < $scrollRefDiv.attr('data-offset')) {
-			var oldTop 	= parseInt($dynDiv.css('top').replace('px', '')),
-				inMin 	= $parent.attr('data-offset'),
-				inMax 	= parseFloat(inMin) + parseFloat($parent.outerHeight()),
-				outMin 	= 0,
-				outMax 	= $(window).outerHeight() * 0.8,
-				newVal 	= -scrolled.map(inMin, inMax, outMin, outMax) + 'px';
-			$dynDiv.css('margin-top', newVal);
-		} else if (scrolled > $scrollRefDiv.attr('data-offset')) {
-			$dynDiv.css('margin-top', $(window).outerHeight() * 0.8);
-		} else if (scrolled < $parent.attr('data-offset')) {
-			$dynDiv.css('margin-top', 0);
-		}
-	}
+        if (scrolled > $parent.attr('data-offset') && scrolled < $scrollRefDiv.attr('data-offset')) {
+            var oldTop = parseInt($dynDiv.css('top').replace('px', '')),
+                inMin = $parent.attr('data-offset'),
+                inMax = parseFloat(inMin) + parseFloat($parent.outerHeight()),
+                outMin = 0,
+                outMax = $(window).outerHeight() * 0.8,
+                newVal = -scrolled.map(inMin, inMax, outMin, outMax) + 'px';
+            $dynDiv.css('margin-top', newVal);
+        } else if (scrolled > $scrollRefDiv.attr('data-offset')) {
+            $dynDiv.css('margin-top', -$(window).outerHeight() * 0.8);
+        } else if (scrolled < $parent.attr('data-offset')) {
+            $dynDiv.css('margin-top', 0);
+        }
+    }
 
-	/*=================================
-		SHOW CONTENTS ON SCROLL
-	**===============================*/
+    /*=================================
+    	SHOW CONTENTS ON SCROLL
+    **===============================*/
 
-	function showOnScroll() {
-		var page = $("body").attr('class').split(" ")[0],
-			offsets = {
-				home: [0.5, 0.75],
-				italiaIndex: [0, 0.5]
-			};
+    function showOnScroll() {
+        var page = $("body").attr('class').split(" ")[0],
+            offsets = {
+                home: [0.5, 0.75],
+                italiaIndex: [0, 0.5]
+            };
 
-		if(offsets[page]) {
-			$entries.each(function(ind, el) {
-				var $entry = ind !== 0 ? $entries.eq(ind - 1) : $entries.eq(0),
-					$scrollStart = ind !== 0 ? parseFloat($entry.attr('data-offset')) + (parseFloat($entry.attr('data-height')) * offsets[page][0]) : 0,
-					$scrollEnd = ind !== 0 ? parseFloat($entry.attr('data-offset')) + (parseFloat($entry.attr('data-height')) * offsets[page][1]) : 0,
-					$opacity;
-				if(scrollPos > $scrollStart && scrollPos < $scrollEnd) {
-					$opacity = ((scrollPos - $scrollStart) / ($scrollEnd - $scrollStart)).toFixed(2);
-				} else if(scrollPos <= $scrollStart) {
-					$opacity = 0;
-				} else if(scrollPos >= $scrollEnd) {
-					$opacity = 1;
-				}
-				$entries.eq(ind).find(".scrollHide").css('opacity', $opacity);
-			});
-		}
-	}
+        if (offsets[page]) {
+            $entries.each(function(ind, el) {
+                var $entry = ind !== 0 ? $entries.eq(ind - 1) : $entries.eq(0),
+                    $scrollStart = ind !== 0 ? parseFloat($entry.attr('data-offset')) + (parseFloat($entry.attr('data-height')) * offsets[page][0]) : 0,
+                    $scrollEnd = ind !== 0 ? parseFloat($entry.attr('data-offset')) + (parseFloat($entry.attr('data-height')) * offsets[page][1]) : 0,
+                    $opacity;
+                if (scrollPos > $scrollStart && scrollPos < $scrollEnd) {
+                    $opacity = ((scrollPos - $scrollStart) / ($scrollEnd - $scrollStart)).toFixed(2);
+                } else if (scrollPos <= $scrollStart) {
+                    $opacity = 0;
+                } else if (scrollPos >= $scrollEnd) {
+                    $opacity = 1;
+                }
+                $entries.eq(ind).find(".scrollHide").css('opacity', $opacity);
+            });
+        }
+    }
 
-	/*=================================
-		AUTOPLAY ANIMATE IN
-	**===============================*/
+    /*=================================
+    	AUTOPLAY ANIMATE IN
+    **===============================*/
 
-	if($("main").hasClass("autoplay") && !$("main").hasClass("home")) {
-		$('html, body').animate({
-			scrollTop: $("#v-header").outerHeight(true)
-		}, 200);
-	}
+    if ($("main").hasClass("autoplay") && !$("main").hasClass("home")) {
+        $('html, body').animate({
+            scrollTop: $("#v-header").outerHeight(true)
+        }, 200);
+    }
 
-	/*=================================
-		EVENT LISTENERS
-	**===============================*/
+    /*=================================
+    	EVENT LISTENERS
+    **===============================*/
 
-	//Keep cards at top of page / Control is-scrollable scrolling
-	window.addEventListener('scroll', function () {
-		this.requestAnimationFrame(function () {
-			scrollPos = window.pageYOffset;
-			//If we're not on mobile
-			if($(window).outerWidth() > 480) {
-				$entries.each(function (ind, el) {
-					var top = parseFloat($(this).attr('data-offset')) + parseFloat($(this).attr('data-height'));
+    //Keep cards at top of page / Control is-scrollable scrolling
+    window.addEventListener('scroll', function() {
+        this.requestAnimationFrame(function() {
+            scrollPos = window.pageYOffset;
+            //If we're not on mobile
+            if ($(window).outerWidth() > 480) {
+                $entries.each(function(ind, el) {
+                    var top = parseFloat($(this).attr('data-offset')) + parseFloat($(this).attr('data-height'));
 
-					if(scrollPos < $(this).attr('data-offset')){//Fixed
-						$(this).removeClass('is-scrollable');
-						$(this).removeClass('fixedTop');
-						$(this).css('top', 0);
-					} else if(scrollPos > $(this).attr('data-offset') && scrollPos < top) {//Scroll
-						$(this).addClass('is-scrollable');
-						$(this).removeClass('fixedTop');
-						$(this).css({
-							'top': $(this).attr('data-offset')+'px',
-							'position': null//Remove any js position
-						});
-					} else if (scrollPos > top) {//Stick-to-top
-						$(this).removeClass('is-scrollable');
-						$(this).addClass('fixedTop');
-					}
-					//Original implementation if the bugs become a problem.
-					//$(this).toggleClass('is-scrollable', scrollPos > $(this).attr('data-offset'))
-				});
-			}
-			if($('body').hasClass('home')) {
-				parallax('#home-videos');
-			}
-			if($('body').hasClass('v')) {
-				parallax('#v-intro');
-			}
-			//Don't show on scroll on team page.
-			if(!$('body').hasClass("team")) showOnScroll();
-		});
-	});
+                    if (scrollPos < $(this).attr('data-offset')) { //Fixed
+                        $(this).removeClass('is-scrollable');
+                        $(this).removeClass('fixedTop');
+                        $(this).css('top', 0);
+                    } else if (scrollPos > $(this).attr('data-offset') && scrollPos < top) { //Scroll
+                        $(this).addClass('is-scrollable');
+                        $(this).removeClass('fixedTop');
+                        $(this).css({
+                            'top': $(this).attr('data-offset') + 'px',
+                            'position': null //Remove any js position
+                        });
+                    } else if (scrollPos > top) { //Stick-to-top
+                        $(this).removeClass('is-scrollable');
+                        $(this).addClass('fixedTop');
+                    }
+                    //Original implementation if the bugs become a problem.
+                    //$(this).toggleClass('is-scrollable', scrollPos > $(this).attr('data-offset'))
+                });
+            }
+            if ($('body').hasClass('home')) {
+                parallax('#home-videos');
+            }
+            if ($('body').hasClass('v')) {
+                parallax('#v-intro');
+            }
+            //Don't show on scroll on team page.
+            if (!$('body').hasClass("team")) showOnScroll();
+        });
+    });
 
-	//Cards bounce in on page load
-	if(!$("main").hasClass("autoplay")) {
-		$("[class*='-section']").animateCss("bounceInUp");
-	}
+    //Cards bounce in on page load
+    if (!$("main").hasClass("autoplay")) {
+        $("[class*='-section']").animateCss("bounceInUp");
+    }
 });
